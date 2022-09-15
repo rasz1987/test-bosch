f9a45de59bb39af3d498f063295a98c0e3c4d0af
rasz1987 <rodolfosifontes1@gmail.com>
2022-09-15 13:28:05 +0100

    update search and delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,14 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { FC } from 'react';
-import { urlList } from './constants/urls';
-import Grid from '@mui/material/Grid';
-import Header from './components/header/Header';
-import Customer from './pages/Customer';
-import Customers from './pages/Customers';
-import NotFound from './pages/NotFound';
+import CustomerProvider from './context/CustomersContext'
+import Main from './pages/Main';
 
-const App: FC = () => {
-  return (
-    <BrowserRouter >
-      <Grid container>
-        <Header />
-        <Routes>
-          <Route path={urlList.customers} element={<Customers />} />
-          <Route path={urlList.customer} element={<Customer />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </Grid>
-    </BrowserRouter>
-  );
-}
+const App: FC = () => (
+  <BrowserRouter >
+    <CustomerProvider>
+      <Main />
+    </CustomerProvider>
+  </BrowserRouter>
+);
 
 export default App;
